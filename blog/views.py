754e59df5a711317ceacc5e14c559f7c44c20242
amithuu <amithkulkarni98@gmail.com>
2023-08-07 12:21:00 +0530

    fix: new way for accessing files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# this was the one way were we can use the apps to open[using HttpResponce]
+
+# def home(request):
+#     return HttpResponse("<h1> hi this is home page </h1>")
+
+# def about(request):
+#     return HttpResponse("<h1> hi this is about page </h1>")
+
+# an another way using render
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request,'blog/about.html')
