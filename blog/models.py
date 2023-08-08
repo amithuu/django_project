791b8cf5cff79a69925d02d7c1c20d8f22f4a1ec
amithuu <amithkulkarni98@gmail.com>
2023-08-08 18:52:12 +0530

    feat: learnt database connection ,migrations,stuffs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Each class will be its own table in the database..
+
+# creating a post field which is creating a attributes inside the table..
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
