dd75bab2d38acd2c2099ce0a58d09c00b3bcee04
amithuu <amithkulkarni98@gmail.com>
2023-08-14 18:33:22 +0530

    feat: created  profile page and learned how to add new files and images, and to get the detailsw automatically flow through other pages.

--- users/forms.py
+++ users/forms.py
@@ -4,10 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    phone_number = forms.CharField()
 
     class Meta:
         model = User
-        fields = ['username', 'email','password1', 'password2'] 
-
-
+        fields = ['username', 'email','phone_number' ,'password1', 'password2'] 
 
