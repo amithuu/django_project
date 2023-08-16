ff03df3ff55b17fe956201384cf489bd584796b7
amithuu <amithkulkarni98@gmail.com>
2023-08-16 16:49:02 +0530

    feat: learnt how to resize image.  Added the new form in profile to update the details.  [Todo: phone_number feild is not getting diaplayed by default ] Added the image at the home page for every user post

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +11,16 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email','phone_number' ,'password1', 'password2'] 
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    phone_number = forms.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number']
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image']
